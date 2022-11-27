434a3c887864f427ffbdb89a78fe9111f5b6a27a
Alex Aguirre <teamaguirre@gmail.com>
2022-11-26 19:09:12 -0800

    created app factory,blueprints, and template view

--- app.py
+++ app.py
@@ -1,18 +1,2 @@
-#configuration
-from flask import Flask
-app =Flask(__name__)
-
-#index route
-@app.route('/')
-def index():
-    return 'Hello, this is Petfax'
-
-#pets route
-@app.route('/pets')
-def pets():
-    return ('Check out our pets available for adoption')
-
-#about route
-@app.route('/about')
-def about():
-    return ('About PetFax page')
+from petfax import create_app
+app=create_app()
