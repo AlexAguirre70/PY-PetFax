c370cccbad63494a65a712ec47c36026701005c6
Alex Aguirre <teamaguirre@gmail.com>
2022-11-28 16:18:24 -0800

    Created the Show view template and linked in index

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,3 +8,13 @@ bp=Blueprint('pet',__name__,url_prefix='/pets')
 @bp.route('/')
 def index():
     return render_template('index.html',pets=pets_data)
+
+@bp.route('/<int:index>')
+def show(index):
+    pet_data= pets_data[int(index)-1]
+    return render_template('show.html',pet=pet_data)
+
+@bp.route('/<int:index>/new')
+def new(index):
+    pet_data= pets_data[int(index)-1]
+    return render_template('new.html',pet=pet_data)
