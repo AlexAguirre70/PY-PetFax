c377c8469240076e7113296ad6540dba089b85a8
Alex Aguirre <teamaguirre@gmail.com>
2022-11-29 14:49:33 -0800

    created DB model, and intalled Migrate and postgres interpreter packages

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import (Flask, redirect)
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
 
+    #add SQLAlchemy config
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://petfax_user:petfax$123@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+    # import the database models
+    from . import models
+    models.db.init_app(app)
+
+    #instantiate Migrate
+    migrate= Migrate(app,models.db)
     @app.route('/')
     def hello ():
-        return 'Hello, PetFax'
+        return redirect('/pets')
 
     from . import pet
     app.register_blueprint(pet.bp) 
