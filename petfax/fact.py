97d8f1810ac200f30b7386cc9796f6be42fe0106
Alex Aguirre <teamaguirre@gmail.com>
2022-11-29 10:10:25 -0800

    created the POST and GET index routes, and Facts Index view

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,10 +1,19 @@
-from flask import (Blueprint, render_template,request)
+from flask import (Blueprint, render_template,request,redirect)
 
 
 bp=Blueprint('fact',__name__,url_prefix='/facts')
 
+#index get and post route
+@bp.route('/', methods=["GET","POST"])
+def index():
+    if request.method =="POST":
+        print(request.form)
+        return redirect('/facts')
+
+    return render_template('facts/index.html')
+
 #new fact form route
 @bp.route('/new')
-def index():
+def new_fact():
     print (request)
     return render_template('facts/new.html')
