a3b10f6928ef7285ff65e7dbc6fd2c1c3f8abbfa
Alex Aguirre <teamaguirre@gmail.com>
2022-11-28 20:46:20 -0800

    Moved the views into subfolders

--- petfax/pet.py
+++ petfax/pet.py
@@ -7,14 +7,10 @@ bp=Blueprint('pet',__name__,url_prefix='/pets')
 
 @bp.route('/')
 def index():
-    return render_template('index.html',pets=pets_data)
+    return render_template('pets/index.html',pets=pets_data)
 
 @bp.route('/<int:index>')
 def show(index):
     pet_data= pets_data[int(index)-1]
-    return render_template('show.html',pet=pet_data)
+    return render_template('pets/show.html',pet=pet_data)
 
-@bp.route('/<int:index>/new')
-def new(index):
-    pet_data= pets_data[int(index)-1]
-    return render_template('new.html',pet=pet_data)
