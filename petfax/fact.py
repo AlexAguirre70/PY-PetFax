aabe0c86d6489475936a4e9c531227c5b7b553e3
Alex Aguirre <teamaguirre@gmail.com>
2022-11-29 16:23:17 -0800

    dynamically rendered the UserFax view from DB

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,5 +1,5 @@
 from flask import (Blueprint, render_template,request,redirect)
-
+from . import models
 
 bp=Blueprint('fact',__name__,url_prefix='/facts')
 
@@ -7,10 +7,18 @@ bp=Blueprint('fact',__name__,url_prefix='/facts')
 @bp.route('/', methods=["GET","POST"])
 def index():
     if request.method =="POST":
-        print(request.form)
-        return redirect('/facts')
+        newFact = request.form['fact']
+        newSubmitter= request.form['submitter']
 
-    return render_template('facts/index.html')
+        new_fact = models.Fact(submitter=newSubmitter, fact=newFact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+    #query the database to get all the rows in facts table
+    results= models.Fact.query.all()
+    
+    return render_template('facts/index.html',facts=results)
 
 #new fact form route
 @bp.route('/new')
